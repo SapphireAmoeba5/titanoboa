d5c6c08cfe89e9ca650fe7515be1427fe551be48
SapphireAmoeba5 <clevermail19@gmail.com>
2022-04-26 12:55:09 -0300

    Test commit on remote pc

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,10 @@ struct Arguments {
 
 fn main() {
     simple_logger::init_with_level(log::Level::Trace);
-    //let args = Arguments::parse();
+    let args = Arguments::parse();
 
-    interpreter::test_from_main();
+    let mut source = std::fs::read_to_string(&args.input_file)
+         .expect(&format!("Failed to open file: {}", args.input_file));
 
-    // let mut source = std::fs::read_to_string(&args.input_file)
-    //     .expect(&format!("Failed to open file: {}", args.input_file));
-
-    // let mut interpreter = Interpreter::run(source);
+    let mut interpreter = Interpreter::run(source);
 }
